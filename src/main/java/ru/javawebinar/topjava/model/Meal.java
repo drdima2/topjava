894bc95a2d73f134cfd13647945c53750349296e
drdima2 <drdima2@gmail.com>
2019-09-29 21:14:09 -0500

    HW0 done

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -2,28 +2,38 @@ package ru.javawebinar.topjava.model;
 
 import java.time.LocalDateTime;
 
-public class Meal {
+public class UserMealWithExceed {
     private final LocalDateTime dateTime;
 
     private final String description;
 
     private final int calories;
 
-    public Meal(LocalDateTime dateTime, String description, int calories) {
+    private final boolean exceed;
+
+
+    public UserMealWithExceed(LocalDateTime dateTime, String description, int calories, boolean exceed) {
         this.dateTime = dateTime;
         this.description = description;
         this.calories = calories;
+        this.exceed = exceed;
     }
 
-    public LocalDateTime getDateTime() {
-        return dateTime;
+    public UserMealWithExceed(UserMeal userMeal, boolean exceed) {
+        this.dateTime = userMeal.getDateTime();
+        this.description = userMeal.getDescription();
+        this.calories = userMeal.getCalories();
+        this.exceed = exceed;
     }
 
-    public String getDescription() {
-        return description;
-    }
-
-    public int getCalories() {
-        return calories;
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("UserMealWithExceed{");
+        sb.append("dateTime=").append(dateTime);
+        sb.append(", description='").append(description).append('\'');
+        sb.append(", calories=").append(calories);
+        sb.append(", exceed=").append(exceed);
+        sb.append('}');
+        return sb.toString();
     }
 }
